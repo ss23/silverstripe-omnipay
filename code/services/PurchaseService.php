ss23/silverstripe-omnipay

commit 964fb1489ea912d9ce330632b235ceab519d5695

Void when payment fails

code/services/PurchaseService.php
@@ -68,6 +68,7 @@ class PurchaseService extends PaymentService{
 			} else {
 				//handle error
 				$this->createMessage('PurchaseError', $response);
+				$this->payment->Status = 'Void';
 				$gatewayresponse->setMessage(
 					"Error (".$response->getCode()."): ".$response->getMessage()
 				);
@@ -75,6 +76,8 @@ class PurchaseService extends PaymentService{
 			$this->payment->write();
 		} catch (Omnipay\Common\Exception\OmnipayException $e) {
 			$this->createMessage('PurchaseError', $e);
+			$this->payment->Status = 'Void';
+			$this->payment->write();
 			$gatewayresponse->setMessage($e->getMessage());
 		}
 		$gatewayresponse->setRedirectURL($this->getRedirectURL());
@@ -100,11 +103,12 @@ class PurchaseService extends PaymentService{
 			if ($response->isSuccessful()) {
 				$this->createMessage('PurchasedResponse', $response);
 				$this->payment->Status = 'Captured';
-				$this->payment->write();
 				$this->payment->extend('onCaptured', $gatewayresponse);
 			} else {
 				$this->createMessage('CompletePurchaseError', $response);
+				$this->payment->Status = 'Void';
 			}
+			$this->payment->write();
 		} catch (Omnipay\Common\Exception\OmnipayException $e) {
 			$this->createMessage("CompletePurchaseError", $e);
 		}

tests/PurchaseServiceTest.php
@@ -46,7 +46,7 @@ class PurchaseServiceTest extends PaymentTest {
 			'expiryMonth' => '5',
 			'expiryYear' => date("Y", strtotime("+1 year"))
 		));
-		$this->assertEquals("Created", $payment->Status, "is the status has not been updated");
+		$this->assertEquals("Void", $payment->Status, "Payment has been void");
 		$this->assertEquals(1222, $payment->Amount);
 		$this->assertEquals("GBP", $payment->Currency);
 		$this->assertFalse($response->isSuccessful());
@@ -93,7 +93,7 @@ class PurchaseServiceTest extends PaymentTest {
 		$this->assertDOSContains(array(
 			array('ClassName' => 'PurchaseError')
 		), $payment->Messages());
-
+		$this->assertEquals("Void", $payment->Status, "Payment has been void");
 		//TODO:
 			//invalid/incorrect card number/date..lhun check (InvalidCreditCardException)
 			//InvalidRequestException thrown when gateway needs specific parameters
@@ -111,7 +111,7 @@ class PurchaseServiceTest extends PaymentTest {
 		));
 		$this->assertFalse($response->isSuccessful()); //payment has not been captured
 		$this->assertFalse($response->isRedirect());
-		$this->assertSame("Created", $payment->Status);
+		$this->assertEquals("Void", $payment->Status, "Payment has been void");
 
 		//check messaging
 		$this->assertDOSContains(array(
@@ -156,7 +156,7 @@ class PurchaseServiceTest extends PaymentTest {
 		$response = $service->purchase();
 		$this->assertFalse($response->isSuccessful()); //payment has not been captured
 		$this->assertFalse($response->isRedirect()); //redirect won't occur, because of failure
-		$this->assertSame("Created", $payment->Status);
+		$this->assertEquals("Void", $payment->Status, "Payment has been void");
 
 		//check messaging
 		$this->assertDOSContains(array(
@@ -193,8 +193,8 @@ class PurchaseServiceTest extends PaymentTest {
 			array('ClassName' => 'CompletePurchaseRequest'),
 			array('ClassName' => 'CompletePurchaseError')
 		), $payment->Messages());
+		$this->assertEquals("Void", $payment->Status, "Payment has been void");
 	}
-
 
 	public function testNonExistantGateway() {
 		//exception when trying to run functions that require a gateway
@@ -205,13 +205,13 @@ class PurchaseServiceTest extends PaymentTest {
 
 		$this->setExpectedException("RuntimeException");
 		try{
-		$result = $service->purchase();
+			$result = $service->purchase();
 		}catch(RuntimeException $e){
 			$this->markTestIncomplete();
-		$totalNZD = Payment::get()->filter('MoneyCurrency', "NZD")->sum();
-		$this->assertEquals(27.23, $totalNZD);
-		$service->purchase();
-		$service->completePurchase();
+			$totalNZD = Payment::get()->filter('MoneyCurrency', "NZD")->sum();
+			$this->assertEquals(27.23, $totalNZD);
+			$service->purchase();
+			$service->completePurchase();
 			//just to assert that exception is thrown
 			throw $e;
 		}

commit 32011372e1dea1cc8059c25b0b4f504e9862b78b

Critical regression from 964fb14
This breaks onCaptured, since it checks for `TotalOutstanding` via `Payable->TotalPaid()`, which in turn uses `the Order->Payments()` relationship. Since `Status=Captured` isn't written at this point, the result is `>0`, failing the order.

code/services/PurchaseService.php
@@ -104,12 +104,13 @@ class PurchaseService extends PaymentService{
 			if ($response->isSuccessful()) {
 				$this->createMessage('PurchasedResponse', $response);
 				$this->payment->Status = 'Captured';
+				$this->payment->write();
 				$this->payment->extend('onCaptured', $gatewayresponse);
 			} else {
 				$this->createMessage('CompletePurchaseError', $response);
 				$this->payment->Status = 'Void';
+				$this->payment->write();
 			}
-			$this->payment->write();
 		} catch (Omnipay\Common\Exception\OmnipayException $e) {
 			$this->createMessage("CompletePurchaseError", $e);
 		}